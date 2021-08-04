46d2bc6ac8cdf57c87bf1d29f46d521505fb42c4
Julius James <3threej18@gmail.com>
2021-08-04 16:22:15 -0500

    Activities-Module

---
+++ activities/src/main/java/typeconversion-mathorder/Review.java
@@ -0,0 +1,49 @@
+package com.pluralsight.typeconversion.mathorder;
+
+public class Review {
+public static void main(String[] args) {
+		
+		/* TODO Create 4 variable.
+		 * 
+		 */
+		
+		
+	   /*  TODO Math Operators.
+	    * 
+	    */
+	    
+	  /* Print Results
+	   * 
+	   */
+	      
+	    
+	      
+	  /* TODO New Math Operator
+	   * 
+	   */
+	      
+	     
+      /* TODO Add Print Statement
+       * 
+       */
+	     
+	/* TODO Create Variables.
+	 * 
+	 */
+	
+	
+	
+	/* TODO Cast Variables to get.
+	 *  
+	 */
+
+ 	
+	/* TODO Add print Statement previous variable example.
+	 * 
+	 */
+  
+ 
+
+
+}
+}
