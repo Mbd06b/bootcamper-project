5e8b70942abb517c96f2ad0163ad2334dc3ef03e
Matthew Dowell <matthew.b.dowell@accenturefederal.com>
2021-08-28 19:23:04 -0500

    TODO comments for participants added for task 1

--- mgl-webapp/src/main/java/com/organization/mvcproject/MGL_Task1/model/Review.java
+++ mgl-webapp/src/main/java/com/organization/mvcproject/MGL_Task1/model/Review.java
@@ -1,6 +1,11 @@
 package com.organization.mvcproject.MGL_Task1.model;
 
 public class Review {
+	
+	/**
+	 * TODO 1.0 java object member variable naming convention, 
+	 * one member declared here is not object oriented refactor it
+	 */
 	private String reviewBody;
 	private String author;
 	private Integer rating;
