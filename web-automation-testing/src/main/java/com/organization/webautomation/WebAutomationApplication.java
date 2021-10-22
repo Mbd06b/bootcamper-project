8f650e3ac0df4a9eb9157ae5e27db8094d7d266a
Matthew Dowell <mbd06b@gmail.com>
2021-10-22 12:02:47 -0500

    init web automation testing module

---
+++ web-automation-testing/src/main/java/com/organization/webautomation/WebAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.organization.webautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAutomationApplication.class, args);
+	}
+
+}
---
+++ web-automation-testing/src/test/java/com/organization/webautomation/WebAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.organization.webautomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
