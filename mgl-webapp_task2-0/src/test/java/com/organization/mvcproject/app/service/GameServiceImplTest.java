525410eab0c718f872a750a33f905652931328b3
Matthew Dowell <mbd06b@gmail.com>
2021-10-27 12:49:04 -0500

    junit5 setup for mgl-webapp_task2_0

---
+++ mgl-webapp_task2-0/src/test/java/com/organization/mvcproject/app/service/GameServiceImplTest.java
@@ -0,0 +1,80 @@
+package com.organization.mvcproject.app.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.organization.mvcproject.config.MvcConfiguration;
+import com.organization.mvcproject.model.Game;
+import com.organization.mvcproject.service.GameService;
+
+
+@RunWith(JUnitPlatform.class)
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = MvcConfiguration.class)
+@WebAppConfiguration
+@TestInstance(Lifecycle.PER_CLASS)
+class GameServiceImplTest {
+	
+	@Autowired
+	private GameService gameServiceUnderTest;
+	
+	private static Game testGame = createGame(1);
+	
+	private  static final String TEST_GENRE = "Test Genre";
+	private static Game createGame(Integer number) {
+		Game game = new Game();
+		 game.setName("Testing Game Name " + String.valueOf(number));
+		 game.setGenre(TEST_GENRE);
+		 return game;
+	}
+	
+	private static List<Game> gamesToRemoveAfterTest = new ArrayList<>();
+	
+	@BeforeAll
+	@Test
+	void saveGameServiceSavesAndUpdatesGame() {
+		if(gamesToRemoveAfterTest.isEmpty()) {
+			Game game = gameServiceUnderTest.saveGame(testGame);
+			assertNotNull(game.getId());
+			
+			//updates 
+			game.setName("Testing Game Name Updated" );
+			testGame = gameServiceUnderTest.saveGame(game);
+			assertEquals(game, testGame);	
+			gamesToRemoveAfterTest.add(testGame);
+			//the saveGame works, save another game to setup list operation tests
+			gamesToRemoveAfterTest.add(gameServiceUnderTest.saveGame(createGame(2)));
+		}
+	}
+
+	@Test
+  	void retrieveAllGamesServiceReturnsGames() {
+		List<Game> games = gameServiceUnderTest.retrieveAllGames(); 
+		assertNotNull(games);
+		assertTrue(games.size() >= 2 );
+	}
+
+	
+
+	
+	
+	
+	
+
+}
