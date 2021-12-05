ca7aaff4702ec6883fb4bfe53a093ac5390e7c8c
Matthew Dowell <mbd06b@gmail.com>
2021-12-05 14:13:13 -0600

    removed duplicate xml namespace declaration

--- mgl-webapp_task1-0/src/main/java/com/organization/mvcproject/service/GameServiceImpl.java
+++ mgl-webapp_task1-0/src/main/java/com/organization/mvcproject/service/GameServiceImpl.java
@@ -1,84 +1,40 @@
 package com.organization.mvcproject.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.organization.mvcproject.model.Game;
+import com.organization.mvcproject.repository.GameDAOMock;
 
 @Service
 public class GameServiceImpl implements GameService {
 
-	private static Long gameId = new Long(0);
-	private static Long companyId = new Long(0);
-	private static List<Game> games = new ArrayList<Game>();
+	@Autowired
+	private GameDAOMock gameDaoMock;
 
-	static {
-		games = populateGames();
-	}
-
-	private static List<Game> populateGames() {
-
-		Game game1 = new Game();
-		game1.setId(++gameId);
-		game1.setGenre("Sport");
-		game1.setName("Rocket League");
-
-		Game game2 = new Game();
-		game2.setId(++gameId);
-		game2.setGenre("Shooter");
-		game2.setName("Halo 3");
-
-		Game game3 = new Game();
-		game3.setId(++gameId);
-		game3.setGenre("MMORPG");
-		game3.setName("Runescape");
-
-		games.add(game1);
-		games.add(game2);
-		games.add(game3);
-
-		return games;
-	}
 
 	@Override
 	public List<Game> retrieveAllGames() {
-		return games;
+		return gameDaoMock.retrieveAllGames();
 	}
 
 	@Override
 	public Game saveGame(Game game) {
-		game.setId(++gameId);
-		games.add(game);
-		return game;
-	}
-	
-
-	public static Long getGameId() {
-		return gameId;
+		//logic on save 
+		return gameDaoMock.saveGame(game);
 	}
 
-	public static void setGameId(Long gameId) {
-		GameServiceImpl.gameId = gameId;
+	@Override
+	public Boolean deleteGame(Long gameId) {
+		  return gameDaoMock.deleteGameById(gameId); 
 	}
 
-	public static Long getCompanyId() {
-		return companyId;
+	@Override
+	public Game updateGame(Game game) {
+		//logic on update
+		  return gameDaoMock.saveGame(game);
 	}
-
-	public static void setCompanyId(Long companyId) {
-		GameServiceImpl.companyId = companyId;
-	}
-
-	public static List<Game> getGames() {
-		return games;
-	}
-
-	public static void setGames(ArrayList<Game> games) {
-		GameServiceImpl.games = games;
-	}
-	
-	
 
 }
