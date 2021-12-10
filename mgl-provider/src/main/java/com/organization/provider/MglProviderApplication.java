93845965a19b562468469cebee43484367659a62
Matthew Dowell <mbd06b@gmail.com>
2021-12-10 15:25:54 -0600

    mgl-provider init

---
+++ mgl-provider/src/main/java/com/organization/provider/MglProviderApplication.java
@@ -0,0 +1,13 @@
+package com.organization.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MglProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MglProviderApplication.class, args);
+	}
+
+}
