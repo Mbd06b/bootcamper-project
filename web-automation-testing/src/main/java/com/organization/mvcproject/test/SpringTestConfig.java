f3be2ea955b090ed1ed948e36d84a0e931dfaa4b
Matthew Dowell <mbd06b@gmail.com>
2022-01-03 10:29:52 -0600

    Junit5, Selenium with driver manager, Cucumber, spring boot initialized

---
+++ web-automation-testing/src/main/java/com/organization/mvcproject/test/SpringTestConfig.java
@@ -0,0 +1,10 @@
+package com.organization.mvcproject.test;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan(basePackages="com.organization.mvcproject")
+@Configuration 
+public class SpringTestConfig {
+
+}
