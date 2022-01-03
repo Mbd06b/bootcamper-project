bee26580d1e403c5e526b5ef354a9e88ee30a55b
Matthew Dowell <mbd06b@gmail.com>
2022-01-03 10:29:52 -0600

    Junit5, Selenium with driver manager, Cucumber, spring boot initialized

--- web-automation-testing/src/test/java/com/organization/mvcproject/test/MglAutomationTestingApplicationTests.java
+++ web-automation-testing/src/test/java/com/organization/mvcproject/test/MglAutomationTestingApplicationTests.java
@@ -1,13 +1,20 @@
 package com.organization.mvcproject.test;
 
+import java.lang.invoke.MethodHandles;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MglAutomationTestingApplicationTests {
+	
+    private static Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
 	@Test
 	void contextLoads() {
+		logger.debug("Context Loads");
 	}
 
 }
