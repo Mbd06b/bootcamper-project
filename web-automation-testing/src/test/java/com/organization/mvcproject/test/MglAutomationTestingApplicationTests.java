025495d1b138d2fed2610eb2d615663ebb292cf0
Mbd06b <mbd06b@gmail.com>
2022-09-20 08:28:15 -0500

    check-in, trying to trigger failsafe tests within dependency

--- web-automation-testing/src/test/java/com/organization/mvcproject/test/MglAutomationTestingApplicationTests.java
+++ web-automation-testing/src/test/java/com/organization/mvcproject/test/MglAutomationTestingApplicationTests.java
@@ -2,19 +2,20 @@ package com.organization.mvcproject.test;
 
 import java.lang.invoke.MethodHandles;
 
-import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import io.cucumber.java.Before;
 
 @SpringBootTest
 class MglAutomationTestingApplicationTests {
 	
     private static Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
-	@Test
+	@Before
 	void contextLoads() {
 		logger.debug("Context Loads");
-	}
+		logger.info("----------------- Spring Context Initialized for Executing Cucumber Tests ------------------");	}
 
 }
