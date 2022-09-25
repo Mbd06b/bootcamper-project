9faf9a7ea4f15a68f6f012a0fe798f38e81c0269
Mbd06b <mbd06b@gmail.com>
2022-09-25 17:29:11 -0500

    failsafe integration tests are working with framework on review module

--- web-automation-testing/src/main/java/com/organization/mvcproject/test/MglAutomationTestingApplication.java
+++ web-automation-testing/src/main/java/com/organization/mvcproject/test/MglAutomationTestingApplication.java
@@ -1,13 +1,13 @@
-package com.organization.mvcproject.test;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class MglAutomationTestingApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MglAutomationTestingApplication.class, args);
-	}
-
-}
+//package com.organization.mvcproject.test;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class MglAutomationTestingApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(MglAutomationTestingApplication.class, args);
+//	}
+//
+//}
