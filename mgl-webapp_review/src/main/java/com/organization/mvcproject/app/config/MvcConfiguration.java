ba41d89050c913abf86e39669b8b4ce42fc11ad9
Mbd06b <mbd06b@gmail.com>
2023-01-29 08:56:00 -0600

    premerge code review

--- mgl-webapp_review/src/main/java/com/organization/mvcproject/app/config/MvcConfiguration.java
+++ mgl-webapp_review/src/main/java/com/organization/mvcproject/app/config/MvcConfiguration.java
@@ -20,7 +20,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Configuration
 @ComponentScan(basePackages="com.organization.mvcproject")
-@PropertySource("classpath:application.properties")
+//@PropertySource("classpath:application.properties")
 @EnableWebMvc
 public class MvcConfiguration implements WebMvcConfigurer {
 	
