a68dce13eaef853a41e58320f0cd23c73aa13bcb
Mbd06b <mbd06b@gmail.com>
2023-02-25 10:16:13 -0600

    bump spring-boot managed dependencies from 2.6.0 -> 2.6.14 including Selenium driver manager > 4.6.0

--- web-automation-testing/src/main/java/com/organization/mvcproject/test/setup/BrowserSetup.java
+++ web-automation-testing/src/main/java/com/organization/mvcproject/test/setup/BrowserSetup.java
@@ -4,13 +4,10 @@ package com.organization.mvcproject.test.setup;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 
 public class BrowserSetup {
@@ -18,10 +15,10 @@ public class BrowserSetup {
 	
 	  WebDriver driver;
 
-	    @BeforeAll
-	    static void setupClass() {
-	        WebDriverManager.chromedriver().setup();
-	    }
+//	    @BeforeAll
+//	    static void setupClass() {
+//	        WebDriverManager.chromedriver().setup();
+//	    }
 
 	    @BeforeEach
 	    void setupTest() {
