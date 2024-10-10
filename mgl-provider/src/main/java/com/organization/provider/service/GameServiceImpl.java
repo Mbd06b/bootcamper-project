c7c22662a5bbfd93ad5281480aeb8188d3fee46f
Mbd06b <mbd06b@gmail.com>
2024-10-10 09:13:30 -0500

    upgrade provider from java 11 to 21

--- mgl-provider/src/main/java/com/organization/provider/service/GameServiceImpl.java
+++ mgl-provider/src/main/java/com/organization/provider/service/GameServiceImpl.java
@@ -2,10 +2,9 @@ package com.organization.provider.service;
 
 import java.util.List;
 
-import javax.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.organization.mvcproject.api.mockdao.GameDAO;
 import com.organization.mvcproject.api.model.Game;
