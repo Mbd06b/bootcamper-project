c409bc3b2d9d38c45ec19863cf095d00bbc99ee0
Mbd06b <mbd06b@gmail.com>
2024-10-09 22:46:23 -0500

    mgl-webapp task 1 updated to java 21

--- mgl-webapp_task1-0/src/main/java/com/organization/mvcproject/config/MGLWebApplicationInitializer.java
+++ mgl-webapp_task1-0/src/main/java/com/organization/mvcproject/config/MGLWebApplicationInitializer.java
@@ -1,12 +1,12 @@
 package com.organization.mvcproject.config;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
-
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRegistration;
 
 public class MGLWebApplicationInitializer implements WebApplicationInitializer {
 
