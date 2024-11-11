3e8f5c1998d9806692e8ac1e01869e6b13537836
Mbd06b <mbd06b@gmail.com>
2024-11-11 16:02:29 -0600

    WIP cucumber-selenium updates with java 21 and boot-managed models.

--- web-automation-testing/src/main/java/com/organization/mvcproject/pom/games/GameRow.java
+++ web-automation-testing/src/main/java/com/organization/mvcproject/pom/games/GameRow.java
@@ -1,5 +1,6 @@
 package com.organization.mvcproject.pom.games;
 
+
 public class GameRow {
 	
 	
---
+++ web-automation-testing/src/test/java/com/organization/mvcproject/test/CucumberSpringConfig.java
@@ -0,0 +1,10 @@
+//package com.organization.mvcproject.test;
+//
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.context.annotation.Import;
+//
+//@SpringBootTest
+//@Import(WebDriverConfig.class)  
+//public class CucumberSpringConfig{
+//
+//}
